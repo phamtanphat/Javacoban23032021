5ff9e7a4adc86eec14fabec49f5d79315a8c81ae
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-03-25 20:19:02 +0700

    add file main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String [] args) throws Exception{
+        System.out.print("Hello Word");
+//        command + shilft + .
+
+        // 1 : Tạo thư mục .git
+    }
+}
