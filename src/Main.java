47e6c1cb8ec3268dad8cd503c3f5917a592a8c44
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-03-25 21:01:12 +0700

    kiểu dữ liệu

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
 
     public static void main(String [] args) throws Exception{
-        System.out.print("Hello Word");
-//        command + shilft + .
+        // 1 : Comment code
+        // ctrl + / , /* */
+        /*
+            System.out.print("Hello Word");
+            System.out.print("Hello Word");
+            System.out.print("Hello Word");
+            System.out.print("Hello Word");
+            System.out.print("Hello Word");
+         */
+        // 2 : Kiểu dữ liệu
+        // Nhóm kiểu dữ liệu nguyên thủy : Không hỗ trợ phương thức xử lý
+        // Nhóm kiểu dữ liệu đối tượng : Hỗ trợ các phương thức xử lý
+//         int , boolean , double , float , char ...
+//        String , Integer, Boolean , Double , Float , CharSequence
 
-        // 1 : Tạo thư mục .git
 
-        //sadasdasd
     }
 }
